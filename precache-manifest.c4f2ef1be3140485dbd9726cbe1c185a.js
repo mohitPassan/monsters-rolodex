c1e9f3d7d4fe7010f34046fca138c8346fe3c371
Mohit Passan <mohitpassan@gmail.com>
2020-01-05 22:10:42 +0530

    Updates

---
+++ precache-manifest.c4f2ef1be3140485dbd9726cbe1c185a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f7c3a0fb46f0dad678b66831e842c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ef12f320325ba2442a48",
+    "url": "/monsters-rolodex/static/css/main.6871ec91.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef12f320325ba2442a48",
+    "url": "/monsters-rolodex/static/js/main.2636a30f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
